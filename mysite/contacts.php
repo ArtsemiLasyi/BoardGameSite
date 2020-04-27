7d852bdf06f7236d8b7aa107ec00018122b37fec
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2020-04-27 12:24:45 +0600

    Лабораторная работа 2
    Исправления

---
+++ mysite/contacts.php
@@ -0,0 +1,16 @@
+
+        <div class="about">
+          <a href="https://vk.com/id0" class="maker">Создатель сайта</a>
+          <br>
+          <figure class="photo">
+            <img src="images/contact.jpg" class="picture">
+          </figure>
+          <br>
+          <h3 class="maker">Телефон: +375290022505</h3><br>
+          <h3 class="maker">VK: randomuser</h3><br>
+          <h3 class="maker">Telegram: @randomuser</h3><br>
+        </div>
+
+
+
+
