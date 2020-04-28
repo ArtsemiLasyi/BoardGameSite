19d9cd0b9d8f149a0fa416b775a27520725ebe17
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2020-04-28 14:34:19 +0600

    Лабораторная работа №3
    Первая версия

---
+++ mysite/games.php
@@ -0,0 +1,28 @@
+        <h2 class="game">Игра престолов</h2>
+        <figure class="photo">
+          <img src="images/GAMEOFTHRONES_LOGO.jpg" class="picture">
+        </figure>
+        <br>
+        <h2 class="game">Каркассон</h2>
+        <figure class="photo">
+          <img src="images/CARCASSONNE_LOGO.jpg" class="picture">
+        </figure>
+        <br>
+        <h2 class="game">Кемет</h2>
+        <figure class="photo">
+          <img src="images/KEMET_LOGO.jpg" class="picture">
+        </figure>
+        <br>
+        <h2 class="game">Корни</h2>
+        <figure class="photo">
+          <img src="images/ROOT_LOGO.jpg" class="picture">
+        </figure>
+        <br>
+        <h2 class="game">Small world</h2>
+        <figure class="photo">
+          <img src="images/SMALLWORLD_LOGO.jpg" class="picture">
+        </figure>
+        <br>
+
+
+
