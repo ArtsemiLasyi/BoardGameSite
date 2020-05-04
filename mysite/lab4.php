14b2a01cc9b95973db882ef803d63048c3d8534b
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2020-05-04 13:08:46 +0600

    Лабораторная работа 4. Вариант 4

---
+++ mysite/lab4.php
@@ -0,0 +1,25 @@
+<?php
+
+//Вариант 4
+
+$arrayRegular = array(
+    'green' => '/([A-ZА-ЯЁ][a-zа-яё]*)/u',                // слова с большой буквы
+    'blue' => '/([0-9]+)/u',                              // целые числа
+    'red' => '/([0-9]+[.,][0-9]{1})\d*/u'                 // дроби
+);
+
+if (isset($_POST['text']) && ($_POST['text'] !== "")) {
+    $text = " " . $_POST['text'] . " ";
+    echo "Исходный текст: " . $text . "<br>";
+    $text = preg_replace($arrayRegular['red'], "<span class=\"red_text\">$1</span>", $text);
+    $text = preg_replace($arrayRegular['green'], "<span class=\"green_text\">$0</span>", $text);
+    $text = preg_replace($arrayRegular['blue'], "<span class=\"blue_text\">$0</span>", $text);
+    echo "Обработанный тeкст:" . $text . "<br>";
+}
+?>
+
+<form method="post" class="form">
+    <textarea name="text" rows="9" cols="50" class="input"></textarea><br>
+    <input type="submit" name="submit" value="Найти все" class="button">
+</form>
+
