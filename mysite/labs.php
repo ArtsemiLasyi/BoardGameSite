fc086a5e7bc706b03eb59837739863e667147964
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2020-05-16 00:29:14 +0300

    Лабораторная работа 8. Вариант 4

--- mysite/labs.php
+++ mysite/labs.php
@@ -1,10 +1,27 @@
+<?php
+
+   include_once('getstatistics.php');
+   include_once('sendmail.php');
+
+
+   if(isset($_GET['cookiename'])) {
+       setcookie($_GET['cookiename'], null, -1);
+       header("Location: labs.php?page=lab6");
+   }
+
+   if(isset($_POST['setcookie'])){
+       include('cookie.php');
+       header("Location: labs.php?page=lab6");
+   }
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
   <head>
     <meta charset="UTF-8" />
     <meta name="description" content="Главная страница сайта" />
     <meta name="keywords" content="Настольня, настольные игры" />
-    <meta http-equiv="refresh" content="60" />
     <meta
       name="viewport"
       content="width=device-width, height=device-height, initial-scale=1.0"
@@ -12,13 +29,20 @@
     <link rel="shortcut icon" href="images/logo.png" type="image/png" />
     <title>Всё и всем про настольные игры</title>
     <?php
+      $variant = 4;
       $fileName='';
       $page = 'page';
       $css = 'css/';
       $main = 'index';
+
       $arrayLabMenu = array(
         'Вторая' => 'lab2',
-        'Третья' => 'lab3'
+        'Третья' => 'lab3',
+        'Четвертая' => 'lab4',
+        'Пятая' => 'lab5',
+        'Шестая'=> 'lab6',
+        'Седьмая' => 'lab7',
+        'Восьмая' => 'lab8'
       );
       $arrayMenu = array(
         'Главная' => $main,
@@ -28,6 +52,8 @@
         'Игры' => 'games',
         'Войти' => 'account'
       );
+
+
       if (isset($_GET[$page])){
         foreach ($arrayLabMenu as $key => $arrayElement) {
           if ($key === $_GET[$page]){
@@ -39,6 +65,7 @@
         $fileName = "labs.css";
       }
     ?>
+
     <link rel="stylesheet" type="text/css" href="<?php echo $css . $fileName; ?>">
     <link href="css/common.css" rel="stylesheet" type="text/css">
   </head>
@@ -52,7 +79,7 @@
         <?php
           foreach ($arrayMenu as $key => $arrayElement) {
             echo '<form method="get" class = "a" action="index.php">';
-            if ((isset($_GET[$page]) && ($key === $_GET[$page]))||((!isset($_GET[$page]))&&($arrayElement === $main))) {
+            if ((isset($_GET[$page]) && ($key === $_GET[$page]))) {
             ?>
               <input type="submit" name="page" class="active_a" value="<?php echo $key; ?>"> 
             <?php
@@ -76,8 +103,14 @@
             }
           }
           if (!isset($_GET[$page])) {
-            echo '<a href="labs.php?page=lab2" class="news">Лабораторная работа №2. Вариант 4</a>';
-            echo '<a href="labs.php?page=lab3" class="news">Лабораторная работа №3. Вариант 4</a>';
+            $i = 2;
+            foreach ($arrayLabMenu as $key => $arrayElement) {
+              ?>
+              <a href="<?php echo 'labs.php?page='. $arrayElement;?>" class="news"><?php echo "Лабораторная работа №" . $i . ". Вариант " . $variant;?></a><br>
+
+        <?php
+              $i++;
+            }
           }
         ?>
       </content>
